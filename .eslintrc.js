41fa3f8a494d70c968d93d5c812202199592b4e3
jsfish <hello_jinyu@163.com>
2018-12-03 21:57:20 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,18 @@ module.exports = {
         "amd": true,
         "es6":true,
     },
+    parser: "babel-eslint",
+    plugins: [
+        'react'
+      ],
     rules:{
         "react/jsx-filename-extension": 0,
-        "no-console":1,
+        "no-console":0,
         'global-require':0,
         'react/no-typos':0,
-        "react/prop-types":0
+        "react/prop-types":0,
+        "no-unused-vars":1,
+        "react/no-array-index-key":0,
+        "react/jsx-one-expression-per-line":0,
     }
 };
